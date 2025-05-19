cb3126ee452c969893a40646ed60fc651f8f17f6
Gabriel Diniz <gabrielpombodiniz@gmail.com>
2025-05-19 15:14:04 -0300

    feat: installing axios

---
+++ lib/axios.ts
@@ -0,0 +1,10 @@
+const axios = require('axios');
+
+const api = axios.create({
+    baseURL: 'http://localhost:8000',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+});
+
+export default api;
