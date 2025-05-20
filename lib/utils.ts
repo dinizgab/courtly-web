9104991ae5aeecffcbd194ad696a559c38668a0b
Gabriel Diniz <gabrielpombodiniz@gmail.com>
2025-05-19 21:30:18 -0300

    feat: add date util functions

--- lib/utils.ts
+++ lib/utils.ts
@@ -2,5 +2,20 @@ import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+    return twMerge(clsx(inputs))
+}
+
+export function getTimeFromDateString(dateString: string): string {
+    const date = new Date(dateString);
+
+    return date.toLocaleTimeString("pt-BR", {
+        hour: "2-digit",
+        minute: "2-digit",
+        hour12: false,
+        timeZone: "UTC",
+    });
+}
+
+export function strToTitle(str: string): string {
+    return str[0].toUpperCase() + str.slice(1).toLowerCase();
 }
