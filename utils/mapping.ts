cd1ca493dfe06d64dd7512a8284bec87ea5f9a14
Gabriel Diniz <gabrielpombodiniz@gmail.com>
2025-06-03 18:53:35 -0300

    feat: add booking mapping

--- utils/mapping.ts
+++ utils/mapping.ts
@@ -1,3 +1,4 @@
+import { Booking, BookingApi } from "@/types/booking"
 import { CourtApi } from "@/types/court"
 
 export function mapCourtApi(api: CourtApi) {
@@ -14,5 +15,21 @@ export function mapCourtApi(api: CourtApi) {
         capacity: api.capacity,
         bookingsToday: 0,
         photos: api.photos ?? [],
+    }
+}
+
+export function mapBookingApi(api: BookingApi): Booking {
+    return {
+        id: api.id,
+        startTime: api.start_time,
+        endTime: api.end_time,
+        totalPrice: api.total_price,
+        court: mapCourtApi(api.court || {} as CourtApi),
+        courtId: api.court_id,
+        guestName: api.guest_name,
+        guestEmail: api.guest_email,
+        guestPhone: api.guest_phone,
+        status: api.status,
+        verificationCode: api.verification_code,
     }
 }
