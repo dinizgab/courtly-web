aa9e5519b1b8a245a698f546364ec7ac989f8c45
Gabriel Diniz <gabrielpombodiniz@gmail.com>
2025-06-03 18:53:23 -0300

    feat: add date utils

---
+++ utils/date.ts
@@ -0,0 +1,29 @@
+import { format, differenceInMinutes } from "date-fns";
+import { ptBR } from "date-fns/locale";
+
+export const formatDatePtBr = (iso: string | Date) =>
+    format(new Date(iso), "dd/MM/yyyy", { locale: ptBR });
+
+export const formatTimePtBr = (iso: string | Date) =>
+    format(new Date(iso), "HH:mm");
+
+export const formatBookingDateTimeString = (
+    startIso: string | Date,
+) => {
+    console.log("startIso", startIso);
+    return `${formatDatePtBr(startIso)} às ${formatTimePtBr(startIso)}`;
+}
+export const formatBookingDuration = (
+    startIso: string | Date,
+    endIso: string | Date,
+) => {
+    const minutes = differenceInMinutes(new Date(endIso), new Date(startIso));
+    if (minutes <= 0) return "00m";
+
+    const h = Math.floor(minutes / 60);
+    const m = minutes % 60;
+    return [h > 0 ? `${h}h` : "", `${m.toString().padStart(2, "0")}m`]
+        .filter(Boolean)
+        .join(" ");
+};
+
