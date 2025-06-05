2029bc627082e241640eb23d7ae599b20508b4aa
Gabriel Diniz <gabrielpombodiniz@gmail.com>
2025-06-05 18:59:05 -0300

    feat: integrate withdraw logic

--- hooks/use-withdrawal.tsx
+++ hooks/use-withdrawal.tsx
@@ -1,23 +1,31 @@
 "use client"
 
+import { useAuth } from "@/contexts/auth-context"
 import api from "@/lib/axios"
 import { useMutation, useQueryClient } from "@tanstack/react-query"
 
 export type WithdrawalStatus = "idle" | "processing" | "completed" | "failed"
 
 export function useWithdrawal() {
-  const qc = useQueryClient()
+    const qc = useQueryClient()
+    const { companyId, token } = useAuth()
 
-  return useMutation<
-    { message: string },
-    Error,
-    { amount: number }
-  >({
-    mutationFn: async ({ amount }) => {
-      const { data } = await api.post("/admin/withdraw", { amount })
-      return data
-    },
-    onSuccess: () => qc.invalidateQueries({ queryKey: ["balance"] }),
-  })
+    // TODO - Implement funcionality to withdraw a specific amount
+    return useMutation<
+        { message: string },
+        Error,
+        { amount: number }
+    >({
+        mutationFn: async ({ amount }) => {
+            const { data } = await api.post(`/admin/companies/${companyId}/withdraw`,
+                {},
+                {
+                    headers: { Authorization: `Bearer ${token}` },
+                },
+            )
+            return data
+        },
+        onSuccess: () => qc.invalidateQueries({ queryKey: ["balance"] }),
+    })
 }
 
